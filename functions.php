2a797af40dfd07176784625670712c12aafc65db
Ary <araceliteruel@gmail.com>
2020-01-16 19:09:25 +0100

    Sidebar with widgets

--- functions.php
+++ functions.php
@@ -44,3 +44,27 @@ function gymfitness_scripts_styles(){
 }
 
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
+
+
+//The widgets
+function gymfitness_widgets(){
+    register_sidebar(array(
+        'name' => 'Sidebar 1',
+        'id' => 'sidebar_1',
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="text-center texto-primario">',
+        'after_title' => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name' => 'Sidebar 2',
+        'id' => 'sidebar_2',
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="text-center texto-primario">',
+        'after_title' => '</h3>'
+    ));
+}
+
+add_action('widgets_init', 'gymfitness_widgets');
