f40fda142c9b4a6105ae192b97cf2de1a4475397
Ary <araceliteruel@gmail.com>
2020-01-17 09:23:36 +0100

    galeria con lightbox

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@
 require get_template_directory() . "/inc/queries.php";
 
 //Cuando el tema es activado
-function gymfitness_setup(){
+function gymfitness_setup()
+{
     //Habilita imagenes destacadas
     add_theme_support('post-thumbnails');
 
@@ -19,26 +20,36 @@ function gymfitness_setup(){
 add_action('after_setup_theme', 'gymfitness_setup');
 
 //Menus de navegación, agregar más utilizando el arreglo
-function gymfitness_menus(){
+function gymfitness_menus()
+{
     register_nav_menus(array(
-        'menu-principal' => __( 'Menu Principal', 'gymfitness' )
+        'menu-principal' => __('Menu Principal', 'gymfitness')
     ));
 }
 
 add_action('init', 'gymfitness_menus');
 
 //Scripts y Styles
-function gymfitness_scripts_styles(){
+function gymfitness_scripts_styles()
+{
     wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
 
     wp_enqueue_style('slicknavCSS', get_template_directory_uri() . '/css/slicknav.min.css', array(), '1.0.10');
 
     wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), '1.0.0');
 
+    if (is_page('galeria')) :
+        wp_enqueue_style('lightboxCSS', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.1');
+    endif;
+
     //hoja de estilos
     wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
 
     wp_enqueue_script('slicknavJS', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.10', true);
+
+    if (is_page('galeria')) :
+        wp_enqueue_script('lightboxJS', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), '2.11.1', true);
+    endif;
 
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'slicknavJS'), '1.0.0', true);
 }
@@ -47,7 +58,8 @@ add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
 
 
 //The widgets
-function gymfitness_widgets(){
+function gymfitness_widgets()
+{
     register_sidebar(array(
         'name' => 'Sidebar 1',
         'id' => 'sidebar_1',
